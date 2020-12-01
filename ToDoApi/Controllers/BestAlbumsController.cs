fe3e98da264d186dc992a1dabf340bcb69010207
davefw <davefw@gmail.com>
2020-12-01 12:58:37 +0000

    Add project files.

---
+++ ToDoApi/Controllers/BestAlbumsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ToDoApi.Models;
+
+namespace ToDoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BestAlbumsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Album> Get()
+        {
+            var album1 = new Album();
+            album1.Artist = "Pink Floyd";
+            album1.Name = "Dark Side of the moon";
+
+            var album2 = new Album()
+            {
+                Artist = "Radiohead",
+                Name = "The Bends"
+            };
+
+            List<Album> albums = new List<Album>
+            {
+                album1,
+                album2
+            };
+
+            return albums;
+        }
+    }
+}
---
+++ ToDoApi/Models/Album.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApi.Models
+{
+    public class Album
+    {
+        public string Artist { get; set; }
+        public string Name { get; set; }
+        DateTime YearPublished { get; set; }
+    }
+}
